7f38accdf869ebd12a83c04111adc44dabee7978
jonaselan <jonas.elan@gmail.com>
2017-07-31 19:52:43 -0300

    Implement the sucess case, return the list of array

--- set_intervals.rb
+++ set_intervals.rb
@@ -1,17 +1,36 @@
 module SetIntervals
+
   def self.intervals(list)
     # should contain unless one item, contain only integer and be ordered
-    if list.empty? || self.only_integer?(list) || self.ordered_list?(list)
+    if list.empty? || self.only_integer?(list) || !self.ordered_list?(list)
       return false
     else
-      return true
+      return split_(list)
     end
   end
 
   def self.only_integer?(list)
     !list.all? {|i| i.is_a?(Integer) }
   end
+
   def self.ordered_list?(list)
-    list.sort == list
+    list.sort === list
   end
+
+  def self.split_(list)
+    result = []
+    new_item = true
+    for i in 0...list.size
+      if new_item
+        result << list[i]
+        new_item = false
+      end
+      if (list[i+1] != list[i]+1) && list[i+1] != nil
+        result << list[i]
+        new_item = true
+      end
+    end
+    result
+  end
+
 end
--- spec/intervalos_spec.rb
+++ spec/intervalos_spec.rb
@@ -4,7 +4,7 @@ describe SetIntervals do
   describe '.intervals' do
     let(:array) { [100, 101, 102, 110, 111, 112, 115] }
     let(:irregular_array) { [100.5] }
-    let(:messy_array) { [99, 100] }
+    let(:cluttered_list) { [100, 99] }
     let(:empty_array) { [] }
 
     context "return false when" do
@@ -15,13 +15,13 @@ describe SetIntervals do
         expect(described_class.intervals(irregular_array)).to be_falsy
       end
       it "the array is not ordered" do
-        expect(described_class.intervals(messy_array)).to be_falsy
+        expect(described_class.intervals(cluttered_list)).to be_falsy
       end
     end
 
     context 'return the list of intervals' do
       it "contain two element of list original" do
-        expect(described_class.intervals(array)).to include([100, 102], [110, 112] [115])
+        expect(described_class.intervals(array)).to match_array([[100, 102], [110, 112], [115]])
       end
     end
   end
