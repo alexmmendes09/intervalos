302e1b474ede6c2d7544cd3412e234a369748a00
jonaselan <jonas.elan@gmail.com>
2017-07-31 18:34:36 -0300

    Create suite test for positive case

--- set_intervals.rb
+++ set_intervals.rb
@@ -1,6 +1,7 @@
 module SetIntervals
   def self.intervals(list)
-    if list.empty? || self.only_integer?(list)
+    # should contain unless one item, contain only integer and be ordered
+    if list.empty? || self.only_integer?(list) || self.ordered_list?(list)
       return false
     else
       return true
@@ -9,5 +10,8 @@ module SetIntervals
 
   def self.only_integer?(list)
     !list.all? {|i| i.is_a?(Integer) }
+  end
+  def self.ordered_list?(list)
+    list.sort == list
   end
 end
--- spec/intervalos_spec.rb
+++ spec/intervalos_spec.rb
@@ -2,16 +2,26 @@ require 'spec_helper'
 
 describe SetIntervals do
   describe '.intervals' do
-    context "return false when" do
-      let(:array) { [100, 101, 102, 110, 111, 112, 115] }
-      let(:irregular_array) { [100.5] }
-      let(:empty_array) { [] }
+    let(:array) { [100, 101, 102, 110, 111, 112, 115] }
+    let(:irregular_array) { [100.5] }
+    let(:messy_array) { [99, 100] }
+    let(:empty_array) { [] }
 
+    context "return false when" do
       it "the array is empty" do
         expect(described_class.intervals(empty_array)).to be_falsy
       end
       it "the array contain unless one no-integer element" do
         expect(described_class.intervals(irregular_array)).to be_falsy
+      end
+      it "the array is not ordered" do
+        expect(described_class.intervals(messy_array)).to be_falsy
+      end
+    end
+
+    context 'return the list of intervals' do
+      it "contain two element of list original" do
+        expect(described_class.intervals(array)).to include([100, 102], [110, 112] [115])
       end
     end
   end
